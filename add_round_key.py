5133bee78dddd2362ba873ee081298d1aaf5ac93
PatricioLugo <patriciolugo1972@gmail.com>
2025-04-21 11:55:26 -0600

    Add_round_key implementation with usage example

---
+++ add_round_key.py
@@ -0,0 +1,34 @@
+def main():
+    current_state = [
+        [0x32, 0x88, 0x31, 0xe0],
+        [0x43, 0x5a, 0x31, 0x37],
+        [0xf6, 0x30, 0x98, 0x07],
+        [0xa8, 0x8d, 0xa2, 0x34]
+    ]
+
+    example_key = [
+        ['2b', '28', 'ab', '09'],
+        ['7e', 'ae', 'f7', 'cf'],
+        ['15', 'd2', '15', '4f'],
+        ['16', 'a6', '88', '3c']
+    ]
+    
+    int_key = [[int(byte, 16) for byte in row] for row in example_key]
+    for row in add_round_key(current_state, int_key):
+        print([f"{byte:02x}" for byte in row])
+
+def add_round_key(state, roundkey):
+    result = []
+
+    for row in range(4):
+        new_row = []
+
+        for col in range(4):
+            xor_byte = state[row][col] ^ roundkey[row][col]
+            new_row.append(xor_byte)
+
+        result.append(new_row)
+
+    return result
+if __name__ == "__main__":
+    main()
