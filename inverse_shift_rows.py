e826ad29a2cf7dffdb0846d200373c93991dd56b
Sergio Rock <rock.sergio.a@gmail.com>
2025-04-26 23:48:27 -0600

    Inverse shift rows function, hex format

---
+++ inverse_shift_rows.py
@@ -0,0 +1,32 @@
+example_state = [[0x19, 0xa0, 0x9a, 0xe9], 
+                 [0xf4, 0xc6, 0xf8, 0x3d], 
+                 [0x8d, 0x48, 0xe3, 0xe2], 
+                 [0x8, 0xbe, 0x2b, 0x2a]]
+
+def inv_shiftrows(state):    
+    # Start state 
+    # [[0x19, 0xa0, 0x9a, 0xe9],   
+    #  [0xf4, 0xc6, 0xf8, 0x3d],    
+    #  [0x8d, 0x48, 0xe3, 0xe2],    
+    #  [0x8, 0xbe, 0x2b, 0x2a]]    
+
+    # End state
+    # [[0x19, 0xa0, 0x9a, 0xe9],   Remains unshifted 
+    #  [0x3d, 0xf4, 0xc6, 0xf8],    Shift 1 to the right
+    #  [0xe3, 0xe2, 0x8d, 0x48],    Shift 2 to the right
+    #  [0xbe, 0x2b, 0x2a, 0x08]]    Shift 3 to the right
+
+    # Second row
+    state[1] = state[1][-1:] + state[1][:-1]
+
+    # Third row 
+    state[2] = state[2][-2:] + state[2][:-2]
+
+    # Fourth row 
+    state[3] = state[3][-3:] + state[3][:-3]
+
+    return state
+
+shifted_inv_state = inv_shiftrows(example_state)
+formatted_inv_state = [[hex(byte) for byte in row] for row in shifted_inv_state]
+print(formatted_inv_state)
