c8b1d321c7d006b224e8bffd545eeb2a9f2093e7
Sergio Rock <rock.sergio.a@gmail.com>
2025-05-08 15:03:50 -0600

    Shift rows 2 con numpy array

--- V2/shift_rows2.py
+++ V2/shift_rows2.py
@@ -1,1 +1,18 @@
+'''
+INPUT: Numpy array 4x4 (state matrix) of integers from sub_bytes
+
+OUTPUT: Numpy array 4x4 (state matrix) of integers 
+'''
+
+import numpy as np
+
+def shift_rows(state): 
+#First row remains unshifted 
+    state[1] = np.roll(state[1],-1) #Second row shifts 1 to the left 
+    state[2] = np.roll(state[2],-2) #Third row shifts 2 to the left 
+    state[3] = np.roll(state[3],-3) #Fourth row shifts 3 to the left 
+
+    return state.T.flatten()  #Transpose and flatten matrix
+
+
 
