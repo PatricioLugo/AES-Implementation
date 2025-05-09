b12d5789fa574c72e9518db62faf4f9e3f54d1d5
PatricioLugo <patriciolugo1972@gmail.com>
2025-05-09 14:25:22 -0600

    Created key formatting function for compatibility

--- V2/add_round_key2.py
+++ V2/add_round_key2.py
@@ -14,15 +14,28 @@ def main():
     ], dtype=np.uint8)
 
     key = np.array([
-        0x2b, 0x7e, 0x15, 0x16,
-        0x28, 0xae, 0xd2, 0xa6,
-        0xab, 0xf7, 0x97, 0x7a,
-        0x8c, 0x3d, 0x1f, 0x00
+        [0x2b, 0x7e, 0x15, 0x16],
+        [0x28, 0xae, 0xd2, 0xa6],
+        [0xab, 0xf7, 0x97, 0x7a],
+        [0x8c, 0x3d, 0x1f, 0x00]
     ], dtype=np.uint8)
 
     new_state = add_round_key(block, key)
     print("New State:")
     print(new_state)
+
+def rearrange_key(key):
+    key = rearrange_key(key)
+    new_key = np.zeros(16, dtype=np.uint8)
+
+    cont = 0
+
+    for i in range(4):
+        for j in range(4):
+            new_key[cont] = key[j][i]
+            cont += 1
+
+    return new_key
 
 def add_round_key(block, key):
 
